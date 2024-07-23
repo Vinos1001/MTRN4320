70c3dc5c6b1da61eb40356d588e179749c1202ec
Vinoshan Parathan <z5358838@ad.unsw.edu.au>
2024-07-23 14:49:57 +1000

    just testing matlab git

---
+++ Lab 2-20240604/Lab2code.m
@@ -0,0 +1,33 @@
+%Lab2 code 
+
+I = imread('TestImage.jpg');
+[ids,locs,detectedFamily] = readArucoMarker(I)
+numMarkers = length(ids);
+for i = 1:numMarkers
+  loc = locs(:,:,i);
+   
+  % Display the marker ID and family
+ % disp("Detected marker ID, Family: " + ids(i) + ", " + detectedFamily(i)); 
+ 
+  % Insert marker edges
+  I = insertShape(I,"polygon",{loc},Opacity=1,ShapeColor="green",LineWidth=4);
+ 
+  % Insert marker corners
+  markerRadius = 6;
+  numCorners = size(loc,1);
+  markerPosition = [loc,repmat(markerRadius,numCorners,1)];
+  markercenter = [(markerPosition(1,1)+markerPosition(3,1))/2, (markerPosition(1,2)+markerPosition(3,2))/2, 6];
+  
+  I = insertShape(I,"FilledCircle",markerPosition,ShapeColor="red",Opacity=1);
+   
+  % Insert marker IDs
+  center = mean(loc);
+  I = insertText(I,center,ids(i),FontSize=30,BoxOpacity=1);
+
+  I = insertShape(I, "filled-circle", markercenter, ShapeColor="cyan", Opacity=1);
+end
+
+tform = fitgeotrans(locs(:,:,8), locs(:,:,4),'projective');
+
+figure(1);
+imshow(I)
